e3d2fecc947c878bb3c3014369ca9b8d1872350f
BAHIRABDULLA <bahirk880@gmail.com>
2024-09-24 09:45:09 +0530

    sign up basic functionality in repo pattern implimented

--- backend/services/userService/src/config/config.ts
+++ backend/services/userService/src/config/config.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectMongodb = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/')
+        await mongoose.connect('mongodb://localhost:27017/vision_user')
         console.log('mongodb connected');
     } catch (error) {
         console.log('failed to connect database',error)
