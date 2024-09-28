fe631432bdf84260bf3c237dfcbc4709fdc1e08b
BAHIRABDULLA <bahirk880@gmail.com>
2024-09-28 11:58:48 +0530

    course cards basic structure implimented

---
+++ frontend/src/components/CourseCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+interface CourseCardProps {
+    title:string;
+    image:string;
+    price:string;
+    rating:string
+}
+
+
+const CourseCard:React.FC<CourseCardProps> = ({title,image,price,rating}) => {
+  return (
+    <div className='bg-gray-800 p-4 rounded-md text-center'>
+        <img src="" className='w-full h-40 object-cover rounded-md' alt="" />
+        <h3 className='mt-4 text-xl font-bold'>{title}</h3>
+        <p className="mt-2 text-purple-400">Price: {price}</p>
+        <p className="mt-1 text-yellow-400">Rating: {rating} ★</p>
+    </div>
+  )
+}
+
+export default CourseCard
