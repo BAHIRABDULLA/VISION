d534cadb1ad0027604145a9c622ca336b129b174
BAHIRABDULLA <bahirk880@gmail.com>
2024-09-29 14:32:01 +0530

    fix:otp verification page ui completed

--- frontend/src/features/user/auth/OtpVerification.tsx
+++ frontend/src/features/user/auth/OtpVerification.tsx
@@ -17,29 +17,29 @@ const OtpVerification: React.FC = () => {
     try {
         const otpString = otp.join('')
         console.log(otpString,'otpstrijng in otp verification ');
-        
+        // const responese = await otpVerify(otpString)
     } catch (err) {
       setError('Sign in failed. Please check your credentials and try again.');
     }
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen flex-col lg:flex-row">
       {/* Left Side with Logo */}
       <div className="flex-1 bg-gray-900 flex items-center justify-center">
-        <img src={vision_logo} alt="" className='' />
+        <img src={vision_logo} alt="vision_logo" className='w-1/2 max-w-xs lg:w-3/4' />
       </div>
 
       {/* Right Side with Form */}
-      <div className="flex-1 flex flex-col items-center mb-8 p-8 ">
-        <h2 className="text-3xl font-semibold ">OTP</h2>
+      <div className="flex-1 flex flex-col items-center justify-center mb-8 p-8 ">
+        <h2 className="text-3xl font-semibold mb-6">OTP Verification</h2>
 
-        <div className="flex flex-col items-center justify-center mt-0 w-full ">
+        <div className="flex flex-col items-center justify-center mt-4 w-full ">
           {/* otp */}
           <Otp  otp={otp} setOtp={setOtp} />
         </div>
 
-        <Button text="SUBMIT" onClick={handleSubmit}  customClasses="bg-gradient-to-r from-pink-500 to-purple-600 w-1/2 mt-3" />
+        <Button text="SUBMIT" onClick={handleSubmit}  customClasses="mt-8 bg-gradient-to-r from-pink-500 to-purple-600 w-full lg:w-1/2 mt-3" />
 
         {/* Error Message */}
         {error && <p className="text-red-500 mt-4">{error}</p>}
