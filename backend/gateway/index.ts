07c30c3ca0a1a24eb57e81a2372796766473cb4b
BAHIRABDULLA <bahirk880@gmail.com>
2024-10-05 16:32:10 +0530

    -- api gateway implimented --

---
+++ backend/gateway/index.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import {createProxyMiddleware} from 'http-proxy-middleware'
+import dotenv from 'dotenv'
+
+const app =  express()
+dotenv.config()
+app.use(cors())
+const targets = {
+    user:process.env.USER_API_BASE_URL,
+    mentor:process.env.MENTOR_API_BASE_URL,
+    admin:process.env.ADMIN_API_BASE_URL,
+    course:process.env.COURSE_API_BASE_URL
+}
+
+app.use('/',createProxyMiddleware({
+    target:'http://localhost:4001',
+    changeOrigin:true
+}))
+app.use('/mentor',createProxyMiddleware({
+    target:'http://localhost:4002',
+    changeOrigin:true
+}))
+app.use('/admin',createProxyMiddleware({
+    target:'http://localhost:4003',
+    changeOrigin:true
+}))
+app.use('/course',createProxyMiddleware({
+    target:targets.course,
+    changeOrigin:true
+}))
+const port = process.env.GATEWAY_PORT
+app.listen(port,()=>console.log(`server running on http://localhost:${port}`))
